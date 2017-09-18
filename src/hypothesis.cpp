9cf2bcf24a629043cb111facdecc2a2599159eec
ajay0 <ajaykrishnak0@gmail.com>
2017-09-19 01:06:20 +0530

    hypothesis: Implement = operator and << operator. Also change parameters to references

--- src/hypothesis.cpp
+++ src/hypothesis.cpp
@@ -1,8 +1,9 @@
-#include <hypothesis.h>
+#include "hypothesis.h"
 
 /*
 	Precondition: length > 0
 */
+using std::cout;
 Hypothesis::Hypothesis(int length) {
 	els = new std::string [length];
 	this -> length = length;
@@ -19,10 +20,22 @@ std::string& Hypothesis::operator [](int index) {
 	return els[index];
 }
 
+bool Hypothesis::operator =(std::initializer_list<std::string> els) {
+	if (length != els.size()) {
+		return false;
+	}
+	int i = 0;
+	for (auto el: els) {
+		(this -> els)[i] = el;
+		++i;
+	}
+	return true;
+}
+
 /*
 	Precondition: Length of both hypotheses are equal
 */
-bool operator >=(Hypothesis a, Hypothesis b) {
+bool operator >=(const Hypothesis& a, const Hypothesis& b) {
 	for (int i = 0; i < a.length; i++) {
 		if ((a.els[i] != match_all && a.els[i] != match_none &&
 			a.els[i] != b.els[i] && b.els[i] != match_none) || 
@@ -33,11 +46,11 @@ bool operator >=(Hypothesis a, Hypothesis b) {
 	return true;
 }
 
-bool operator >(Hypothesis a, Hypothesis b) {
+bool operator >(const Hypothesis& a, const Hypothesis& b) {
 	return (a >= b && !(a <= b));
 }
 
-bool operator <=(Hypothesis a, Hypothesis b) {
+bool operator <=(const Hypothesis& a, const Hypothesis& b) {
 	for (int i = 0; i < a.length; i++) {
 		if ((a.els[i] != match_all && a.els[i] != match_none &&
 			a.els[i] != b.els[i] && b.els[i] != match_all) || 
@@ -48,6 +61,14 @@ bool operator <=(Hypothesis a, Hypothesis b) {
 	return true;
 }
 
-bool operator <(Hypothesis a, Hypothesis b) {
+bool operator <(const Hypothesis& a, const Hypothesis& b) {
 	return (a <= b && !(a >= b));
+}
+
+std::ostream& operator <<(std::ostream& out, const Hypothesis& h) {
+	for (int i = 0; i < h.length - 1; i++) {
+		out << h.els[i] << " ";
+	}
+	out << h.els[h.length - 1];
+	return out;
 }
--- src/hypothesis.h
+++ src/hypothesis.h
@@ -2,6 +2,7 @@
 #define HYPOTHESIS_H_
 
 #include <string>
+#include <iostream>
 
 // static so that the variables are local to the compilation unit
 static const std::string match_all = "\n";
@@ -11,11 +12,16 @@ class Hypothesis {
 public:
   Hypothesis(int length);
   ~Hypothesis();
+  
   std::string& operator [](int index);
-  friend bool operator >=(Hypothesis a, Hypothesis b);
-  friend bool operator >(Hypothesis a, Hypothesis b);
-  friend bool operator <=(Hypothesis a, Hypothesis b);
-  friend bool operator <(Hypothesis a, Hypothesis b);
+  bool operator =(std::initializer_list<std::string> els);
+  
+  friend bool operator >=(const Hypothesis& a, const Hypothesis& b);
+  friend bool operator >(const Hypothesis& a, const Hypothesis& b);
+  friend bool operator <=(const Hypothesis& a, const Hypothesis& b);
+  friend bool operator <(const Hypothesis& a, const Hypothesis& b);
+  friend std::ostream& operator <<(std::ostream& out, const Hypothesis& h);
+
 private:
   std::string *els;
   int length;
