ccd404e3c2bceeb82b6f3d7ccc3e533506426e35
nikhil <nikhil>
2017-10-02 14:53:07 +0000

    added comments and made necessary changes

--- src/code.cpp
+++ src/code.cpp
@@ -4,33 +4,31 @@
 
 using namespace std;
 
-vector<Hypothesis> general,specific;
+vector<Hypothesis> general,specific;										// vector of classes for general and specific boundary
 pair< vector<Hypothesis>, vector<Hypothesis> > boundary[8];
-vector< vector <string> > input_data[8]; 
-vector< vector< string> > input_attr = readAttr();
+vector< vector <string> > input_data[8];
+vector< vector< string> > input_attr = readAttr();  		// function to read the input data 7 times for each class
 const int num_attr = 16;
 
-void find_target(int idx)
+void find_target(int idx)																			// function to find target hypothesis
 {
 	general.clear();	specific.clear();
-	general.push_back(Hypothesis(num_attr,match_all));	
-	specific.push_back(Hypothesis(num_attr,match_none));
-	//cout<<general[0]<<"          "<<specific[0]<<endl;
+	general.push_back(Hypothesis(num_attr,match_all));					// initial general boundary
+	specific.push_back(Hypothesis(num_attr,match_none)); 				// initial specific boundary
+
 	for(vector<vector<string> >:: iterator it=input_data[idx].begin();it!=input_data[idx].end();it++)
 	{
-		vector<string> instance = *it,temp;	
+		vector<string> instance = *it,temp;
 
 		for(int i=0;i<instance.size();i++)	if(i!=0 && i!=instance.size()-1)	temp.push_back(instance[i]);
-		int target_val = atoi((instance[instance.size()-1]).c_str());
-		
-		//rep(i,0,temp.size())	cout<<temp[i]<<endl;
-		
-		if(target_val==1)
+		int target_val = atoi((instance[instance.size()-1]).c_str());				// storing the target value
+
+		if(target_val==1)																				// if the example is positive
 		{
 			vector<Hypothesis> temp_hyp;
-			
+
 			for(int i=0;i<general.size();i++)
-				if(general[i].isSatisfy(temp) == target_val)		
+				if(general[i].isSatisfy(temp) == target_val)				// checking for consistency with training example
 					temp_hyp.push_back(general[i]);
 
 			general.clear();
@@ -41,22 +39,22 @@ void find_target(int idx)
 			{
 				if(specific[i].isSatisfy(temp)== target_val)
 					temp_hyp.push_back(specific[i]);
-				else 
+				else
 				{
 					vector<string> min_gen;
 					for(int j=0;j<num_attr;j++)
-					{	
+					{
 						string s= specific[i][j];
 						if(s == match_none)		s=temp[j];
-						else if(s!=temp[j])		s="?";
+						else if(s!=temp[j])		s="?";								// generalising the specific boundary
 						min_gen.push_back(s);
 					}
-					
+
 					int flag=0;
 					if(Hypothesis(num_attr,min_gen).isSatisfy(temp)== target_val)
 					{
-						for(int j=0;j<general.size();j++)		
-							if(general[j] > Hypothesis(num_attr,min_gen)) 	
+						for(int j=0;j<general.size();j++)
+							if(general[j] > Hypothesis(num_attr,min_gen))
 							{
 								flag=1;
 								break;
@@ -64,18 +62,18 @@ void find_target(int idx)
 					}
 					if(flag==1)		temp_hyp.push_back(Hypothesis(num_attr,min_gen));
 
-				}
 			}
+		}
 			specific.clear();
 			specific = temp_hyp;
 		}
 
-		else
+		else																							// if the example is negative
 		{
 			vector<Hypothesis> temp_hyp;
 
-			for(int i=0;i<specific.size();i++)		
-				if(specific[i].isSatisfy(temp) == target_val)	
+			for(int i=0;i<specific.size();i++)
+				if(specific[i].isSatisfy(temp) == target_val)
 					temp_hyp.push_back(specific[i]);
 
 			specific.clear();
@@ -84,15 +82,15 @@ void find_target(int idx)
 
 			for(int i=0;i<general.size();i++)
 			{
-				if(general[i].isSatisfy(temp) == target_val)	
+				if(general[i].isSatisfy(temp) == target_val)
 					temp_hyp.push_back(general[i]);
-				else
+				else																					// specializing the general boundary
 				{
 					vector<Hypothesis> temp_hyp_2;
 					vector<string> min_spf;
 					for(int j=0;j<num_attr;j++)		min_spf.push_back(general[i][j]);
 
-					for(int j=0;j<num_attr;j++)
+					for(int j=0;j<num_attr;j++)			//minimum specialization of the inconsistent hypothesis
 					{
 						if(min_spf[j]==match_all)
 						{
@@ -112,22 +110,22 @@ void find_target(int idx)
 						{
 							string s= min_spf[j];
 							min_spf[j]=match_none;
-							temp_hyp_2.push_back(Hypothesis(num_attr,min_spf));	
+							temp_hyp_2.push_back(Hypothesis(num_attr,min_spf));
 							min_spf[j]=s;
 						}
 					}
 					for(int j=0;j<temp_hyp_2.size();j++)
 					{
-						if(temp_hyp_2[j].isSatisfy(temp) == target_val)
-						{
-							int flag=0;
-							for(int k=0;k<specific.size();k++)
+						if(temp_hyp_2[j].isSatisfy(temp) == target_val)		// checking if newly generated hypothesis is consistent
 							{
-								if(specific[k] < temp_hyp_2[j])
-								{
-									flag=1;
-									break;
-								}
+								int flag=0;
+								for(int k=0;k<specific.size();k++)
+							{
+									if(specific[k] < temp_hyp_2[j])
+									{
+										flag=1;
+										break;
+									}
 							}
 							if(flag==1)	temp_hyp.push_back(temp_hyp_2[j]);
 						}
@@ -140,7 +138,7 @@ void find_target(int idx)
 				int flag=1;
 				for(int j=0;j<temp_hyp.size();j++)
 				{
-					if(temp_hyp[i]<temp_hyp[j])
+					if(temp_hyp[i]<temp_hyp[j])			// checking if any general hypothesis is less general than any other general hypothesis
 					{
 						flag=0;
 						break;
@@ -150,31 +148,33 @@ void find_target(int idx)
 			}
 		}
 	}
-	boundary[idx]= make_pair(specific,general);
+	boundary[idx]= make_pair(specific,general);		// inserting the pair of boundaries
 }
 
 
 int main()
 {
-	for(int i=1;i<8;i++)	input_data[i]=readData();
+	for(int i=1;i<8;i++)	input_data[i]=readData();						// reading the data 7 times
 	for(int i=1;i<8;i++)
 	{
 		for(vector<vector<string> >:: iterator it=input_data[i].begin();it!=input_data[i].end();it++)
 		{
-			int z= atoi(((*it)[(*it).size()-1]).c_str());	
-			if(z == i)	
-				(*it)[(*it).size()-1]="1";
-			else (*it)[(*it).size()-1]="0";
+			int z= atoi(((*it)[(*it).size()-1]).c_str());
+			if(z == i)
+				(*it)[(*it).size()-1]="1";										// changing the target value for each class
+			else (*it)[(*it).size()-1]="0";									// for easier computation
 		}
 	}
 	for(int i=1;i<8;i++)
-	{	
-		find_target(i);
+	{
+		find_target(i);																		// function to generate the boundaries
 		cout<<"Specific Hypothesis of version space "<<i<<endl;
-		for(int i=0;i<specific.size();i++)	cout<<specific[i]<<" ";	
+		if(specific.size()==0)		cout<<"No specific boundary";
+		else for(int i=0;i<specific.size();i++)	cout<<specific[i]<<" ";
 		cout<<endl;
-		cout<<"General Hypothesis of version space "<<i<<endl;
-		for(int i=0;i<general.size();i++)		cout<<general[i]<<" ";
+	  cout<<"General Hypothesis of version space "<<i<<endl;
+		if(general.size()==0)		cout<<"No general boundary";
+		else for(int i=0;i<general.size();i++)		cout<<general[i]<<" ";
 		cout<<endl<<endl<<endl;
 	}
 	return 0;
--- src/hypothesis.cpp
+++ src/hypothesis.cpp
@@ -12,7 +12,7 @@ Hypothesis::Hypothesis(int length, std::string initialize) {
 
 Hypothesis::Hypothesis(int length, std::vector<std::string> initialize) {
 		els = initialize;
-}	
+}
 
 Hypothesis::Hypothesis(const Hypothesis& h) {
 	els = h.els;
@@ -31,8 +31,8 @@ bool Hypothesis::isSatisfy(std::initializer_list<std::string> els) {
 	return ((*this) >= h);
 }
 
-bool Hypothesis::isSatisfy(std::vector<std::string> els) {
-	if ((this -> els).size() != els.size()) {
+bool Hypothesis::isSatisfy(std::vector<std::string> els) {		// function to return boolean value which
+	if ((this -> els).size() != els.size()) {										// checks if the example satisfies the hypothesis
 		return false;
 	}
 	Hypothesis h;
@@ -60,10 +60,10 @@ Hypothesis Hypothesis::operator =(std::vector<std::string> els) {
 /*
 	Precondition: Length of both hypotheses are equal
 */
-bool operator >=(const Hypothesis& a, const Hypothesis& b) {
+bool operator >=(const Hypothesis& a, const Hypothesis& b) {	//	">=" operator overloaded for ease of comparison
 	for (int i = 0; i < a.els.size(); i++) {
 		if ((a.els[i] != match_all && a.els[i] != match_none &&
-			a.els[i] != b.els[i] && b.els[i] != match_none) || 
+			a.els[i] != b.els[i] && b.els[i] != match_none) ||
 			(a.els[i] == match_none && b.els[i] != match_none)) {
 			return false;
 		}
@@ -71,14 +71,14 @@ bool operator >=(const Hypothesis& a, const Hypothesis& b) {
 	return true;
 }
 
-bool operator >(const Hypothesis& a, const Hypothesis& b) {
+bool operator >(const Hypothesis& a, const Hypothesis& b) {		// ">" operator overloaded for ease of comparison
 	return (a >= b && !(a <= b));
 }
 
-bool operator <=(const Hypothesis& a, const Hypothesis& b) {
+bool operator <=(const Hypothesis& a, const Hypothesis& b) {	//	"<=" operator overloaded for ease of comparison
 	for (int i = 0; i < a.els.size(); i++) {
 		if ((a.els[i] != match_all && a.els[i] != match_none &&
-			a.els[i] != b.els[i] && b.els[i] != match_all) || 
+			a.els[i] != b.els[i] && b.els[i] != match_all) ||
 			(a.els[i] == match_all && b.els[i] != match_all)) {
 			return false;
 		}
@@ -86,15 +86,16 @@ bool operator <=(const Hypothesis& a, const Hypothesis& b) {
 	return true;
 }
 
-bool operator <(const Hypothesis& a, const Hypothesis& b) {
+bool operator <(const Hypothesis& a, const Hypothesis& b) {		// "<" operator overloaded for ease of comparison
 	return (a <= b && !(a >= b));
 }
 
-bool operator ==(const Hypothesis& a, const Hypothesis& b) {
+bool operator ==(const Hypothesis& a, const Hypothesis& b) {	// equality operator overloaded
 	return (a <= b && (a >= b));
 }
 
-std::ostream& operator <<(std::ostream& out, const Hypothesis& h) {
+std::ostream& operator <<(std::ostream& out, const Hypothesis& h) {		// "<<" operator overloading for ease of
+																																			//	printing the hypothesis
 	out << "<";
 	for (int i = 0; i < h.els.size() - 1; i++) {
 		out << h.els[i] << ",";
@@ -105,8 +106,8 @@ std::ostream& operator <<(std::ostream& out, const Hypothesis& h) {
 
 
 
-std::vector<std::vector<std::string> > readData(){
-	int n=18;
+std::vector<std::vector<std::string> > readData(){		// function to read input data and store it in
+	int n=18;																						// a vector of strings
 	std::string fileloc="../data/zoo.data";
 	//std::string fileloc="../data/zoo.data";
 	std::ifstream fin(fileloc,std::ios::in);
@@ -118,7 +119,7 @@ std::vector<std::vector<std::string> > readData(){
 		fin>>s;
 
 		std::stringstream str(s);
-		
+
 		for(int i=0;i<n;i++){
 			std::string temp;
 			std::getline(str,temp,',');
@@ -129,7 +130,7 @@ std::vector<std::vector<std::string> > readData(){
 	return data;
 }
 
-std::vector<std::vector<std::string> > readAttr(){
+std::vector<std::vector<std::string> > readAttr(){		// function to read unique values of each attribute
 	std::string fileloc="../data/attr_values";
 	//std::string fileloc="../data/zoo.data";
 	std::ifstream fin(fileloc,std::ios::in);
@@ -143,7 +144,7 @@ std::vector<std::vector<std::string> > readAttr(){
 		{
 			std::string temp;
 			std::getline(str,temp,',');
-			att.push_back(temp);	
+			att.push_back(temp);
 		}
 		data.push_back(att);
 	}
